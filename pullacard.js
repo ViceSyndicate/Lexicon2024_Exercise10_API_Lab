4783cf482593aa69a79039cfae63702b778ba2c5
Victor S <victor.skarerverket@gmail.com>
2024-04-08 03:18:45 +0200

    Saving work on card. Size seems bugged.

--- pullacard.js
+++ pullacard.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('pull').addEventListener('click', makeADeck);
+    document.getElementById('pull').addEventListener('click', pullACard);
+    document.getElementById('createDeck').addEventListener('click', makeADeck);
 });
 
 
-const deck_id = makeADeck;
-pullACard(deck_id);
+var deck_id;
+
 
 function makeADeck(){
     let uri = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
@@ -19,25 +20,39 @@ function makeADeck(){
         console.log(stringify);
         let deckId = data.deck_id;
         console.log(`Deck ID: ${deckId}`);
+        deck_id = deckId;
         return deckId;
     })
     .catch (err => console.log(err))
 }
 
 
-function pullACard(deckId){
-    let uri = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+function pullACard(){
+    let uri = `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`
     console.log('Card Pulled');
     fetch(uri)
     .then(res => res.json())
     .then(data => {
-        console.log(uri);
-        console.log(data);
-        
-        let stringify = JSON.stringify(data, null, 2);
-        console.log(stringify);
+
+        let img = document.createElement('image');
+        img.setAttribute('width', 226);
+        img.setAttribute('height', 314);
+        let imgUrl = data.cards[0].image;
+        img.setAttribute('src', imgUrl);
+
+        let cardDiv = document.getElementById('cardDiv');
+        cardDiv.setAttribute('width', 226);
+        cardDiv.setAttribute('height', 314);
+        cardDiv.appendChild(img);
+
+        for(let i = 0; i < data.cards.length; i++){
+
+        }
+        //let stringify = JSON.stringify(data, null, 2);
+        //console.log(stringify);
+        //document.getElementById('textarea').value = s;
+
         // Count says how many cards to draw
-        // Draw a card
     })
     .catch (err => console.log(err))
 }
